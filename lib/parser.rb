e9815e9acb516cf479e34145c3ee48f5f38acd3b
Ricky Ng <dummey@gmail.com>
2015-07-23 15:00:24 -0500

    Update parser.rb

--- lib/parser.rb
+++ lib/parser.rb
@@ -4,11 +4,14 @@ class Parser
   attr_reader :file
   attr_reader :result
 
+  # This is technically an xml_string and not a file.
   def initialize(file)
     @file = Nokogiri::XML(file)
   end
 
   def speakers_and_lines
+    # I may argue that this would make more since as a .each loop where you add {speaker => lines} into a hash
+    # This would normalize the data that you then use in the next step. Generally speaking, the format of data should be as simple and straight foward as possible when leaving one object to another. 
     search_for(file, "SPEECH").map do |speech| 
       speaker = search_for(speech, "SPEAKER").text
       lines = search_for(speech, "LINE").map(&:text)
@@ -20,6 +23,7 @@ class Parser
 
   private
 
+  # There could be an argument that this method is extraneous and it would be more clear to just see #search above.
   def search_for(source, text)
     source.search(text)
   end
