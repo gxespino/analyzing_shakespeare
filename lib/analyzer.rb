9c901b3e00433bacc82bc6e2f625d512fc444d1f
Ricky Ng <dummey@gmail.com>
2015-07-23 14:43:29 -0500

    code review for analyzer.rb

--- lib/analyzer.rb
+++ lib/analyzer.rb
@@ -7,10 +7,15 @@ class Analyzer
   end
 
   def analyze
-    hash = line_hash_array.reduce(Hash.new, :merge)
-    @sorted_hash = hash.sort_by { |_key, value| value }.reverse.to_h.each do |k, v|
+    #Generally better to use self.foo or @foo so that you don't accidently introduce a variable right above with the same name
+    hash = @line_hash_array.reduce(Hash.new, :merge)
+    
+    #Each line should have a single clear intention. Previously, it would of been "reverse sort and print". When you have an 'and' in your intent, likely you want to break it up
+    @sorted_hash = hash.sort_by { |key, value| value }.reverse.to_h
+    @sorted_hash.each do |k, v|
       puts "#{v} #{k}"
     end
+    
     @sorted_hash
   end
 end
