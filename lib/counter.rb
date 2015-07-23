d807017ed94710b5eace894eb95455bca2c0553a
Ricky Ng <dummey@gmail.com>
2015-07-23 14:52:14 -0500

    Update counter.rb

--- lib/counter.rb
+++ lib/counter.rb
@@ -6,10 +6,14 @@ class Counter
   end
 
   def line_count(speaker)
+    # Shouldn't this actually be calling line_count_hash?
     consolidate_lines[speaker].count
   end
 
   def consolidate_lines
+    # https://github.com/bbatsov/ruby-style-guide
+    # Some argue that reduce should be used over inject
+    # Also confused about this section of code =(
     parsed_hash.inject { |speaker, lines| speaker.merge(lines) { |x, val_1, val_2 | val_1 + val_2 }}
   end
 
